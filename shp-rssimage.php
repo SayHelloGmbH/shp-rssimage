a4082bb655e5876a2aa9cd3d58eb9425cdf51178
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-07-11 10:37:10 +0200

    Add basic file structure

---
+++ shp-rssimage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Add post thumbnail image to RSS feed
+ * Plugin URI:      https://github.com/SayHelloGmbH/shp-rssimage
+ * Description:     Adds the post thumbnail to the RSS feed using the XML tag specified in the Media RSS Specification.
+ * Author:          Say Hello GmbH
+ * Author URI:      https://sayhello.ch/?utm_source=wp-plugin&utm_medium=wp-plugin&utm_campaign=shp-rssimage
+ * Text Domain:     shp-rssimage
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ */
+
+// Your code starts here.
